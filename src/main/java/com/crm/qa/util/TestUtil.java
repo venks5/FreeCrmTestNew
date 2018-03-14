127b084d39fa0d5ebd7368c949f0374cdc0b842e
venky07 <venkypatil05@gmail.com>
2018-03-14 11:02:41 +0530

    Started First MavenDemoProject

---
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -0,0 +1,11 @@
+package com.crm.qa.util;
+
+import com.crm.qa.base.TestBase;
+
+public class TestUtil extends TestBase{
+
+	public static long PAGE_LOAD_TIMEOUT=30;
+	public static long IMPLICIT_WAIT=20;
+	
+	
+}
